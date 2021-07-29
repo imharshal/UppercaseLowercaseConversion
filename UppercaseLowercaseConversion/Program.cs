88600ad273c0503fc48ca373d474d8dbef58bf11
Harshal Bokade <harshalbokade984@gmail.com>
2021-07-29 13:02:40 +0530

    Add project files.

---
+++ UppercaseLowercaseConversion/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace UppercaseLowercaseConversion
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string str;
+            Console.WriteLine("Enter a string");
+            str = Console.ReadLine();
+            Console.WriteLine("In Uppercase: {0}",str.ToUpper());
+            Console.WriteLine("In Lowecase: {0}", str.ToLower());
+
+        }
+    }
+}
